0413638ba262e413e0a64bc0418a3a129cf4d4a9
Dany De Bontridder <dany@alchimerys.be>
2010-12-13 20:26:51 +0000

    Generate writing
    git-svn-id: svn+ssh://ns352270.ovh.net/svn_extension/trunk@70 86b2a579-bf87-4cb0-bb5f-5dc311b2927b

--- amortis/include/am_util.php
+++ amortis/include/am_util.php
@@ -24,6 +24,49 @@
  * \brief let you generate the accounting for the paid off for a selected
  *  year
  */
+require_once('class_am_generate.php');
+require_once('class_amortissement_sql.php');
+global $cn;
+
+$am=new Am_Generate();
+
+if (isset( $_POST['generate'] ))
+  {
+    /*
+     * propose writing
+     */
+    if ( $am->propose_writing($_POST) == false )
+      {
+	echo '<div class="content" style="width:80%;margin-left:10%">';
+	echo $am->input($_POST );
+	echo '</div>';
+      }
+    exit();
+  }
+
+if (isset($_POST['save']))
+  {
+    $ledger=new Acc_Ledger($cn,$_POST['p_jrn']);
+    try 
+      {
+	$ledger->save($_POST);
+
+	$jr_id=$cn->get_value("select jr_id from jrn where jr_internal=$1",array($ledger->internal));
+	echo '<div class="content" style="width:80%;margin-left:10%">';
+
+	echo HtmlInput::detail_op($jr_id,"Opération sauvée : ".$ledger->internal);	
+	echo '</div>';
+
+	exit();
+      }
+    catch (Exception $e)
+      {
+	echo alert($e->getMessage());
+      }
+  }
 
 echo '<div class="content" style="width:80%;margin-left:10%">';
+echo $am->input($_POST );
+
+
 echo '</div>';
---
+++ amortis/include/template/util_generate.php
@@ -0,0 +1,87 @@
+<?php
+/*
+ *   This file is part of PhpCompta.
+ *
+ *   PhpCompta is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   PhpCompta is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with PhpCompta; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+/* $Revision$ */
+
+// Copyright Author Dany De Bontridder ddebontridder@yahoo.fr
+
+/*!\file
+ * \brief select the mat. to amortize, the ledger and the year 
+ */
+ ?>
+<form method="post">
+<?=HtmlInput::hidden('plugin_code',$_REQUEST['plugin_code']);?>
+<?=HtmlInput::hidden('sa',$_REQUEST['sa']);?>
+<?=dossier::hidden()?>
+
+<table>
+<tr>
+<td>Année</td>
+<td><?=$year->input();?></td>
+</tr>
+<tr>
+<td>Journal dans lequel l'écriture sera passée</td>
+<td><?=$sel_ledger->input()?></td>
+</tr>
+<tr>
+<td>Date de l'opération</td>
+<td><?=$p_date->input()?></td>
+</tr>
+<tr>
+<td></td>
+<td></td>
+</tr>
+</table>
+
+Cochez ce qu'il faut amortir 
+<table>
+<tr>
+	<th>Selection</th>
+	<th>Quick Code</th>
+	<th>Nom</th>
+	<th>Description</th>
+	<th>Montant à amortir</th>
+</tr>
+<?
+/*
+ * get all the material
+ */
+$am=new Amortissement_Sql($cn);
+$array=$am->seek ("where a_visible='Y'");
+for ( $i =0;$i<count($array);$i++):
+	echo HtmlInput::hidden('a_id[]',$array[$i]->a_id);
+	$ck=new ICheckBox('p_ck'.$i);
+	$fiche=new Fiche($cn,$array[$i]->f_id);
+	
+?>
+<tr>
+<?
+if ( isset($_POST['p_ck'.$i])) $ck->selected=true;else $ck->selected=false;
+?>
+	<td><?=$ck->input()?></td>
+	<td><?=$fiche->strAttribut(ATTR_DEF_QUICKCODE)?></td>
+	<td><?=$fiche->strAttribut(ATTR_DEF_NAME)?></td>
+	<td>	<?=$fiche->strAttribut(9)?></td>
+	<td></td>
+</tr>
+<?
+endfor;
+?>
+</table>
+<?=HtmlInput::submit('generate',"Générer l'écriture");?>
+</form>
