74f57474579269c4478f596ca1f59e59152bee4a
Dany De Bontridder <dany@alchimerys.be>
2012-01-04 15:47:28 +0000

    schema is coprop
    git-svn-id: svn+ssh://ns352270.ovh.net/svn_extension/trunk@241 86b2a579-bf87-4cb0-bb5f-5dc311b2927b

--- coprop/include/ajax_remove_key.php
+++ coprop/include/ajax_remove_key.php
@@ -1,4 +1,4 @@
 <?php
 global $cn;
-$cn->exec_sql("delete from copro.clef_repartition where cr_id=$1",array($key_id));
+$cn->exec_sql("delete from coprop.clef_repartition where cr_id=$1",array($key_id));
 ?>
