2f72cbf4ef3b3ccb456fe30fd650cc3ea44cbf15
Dany De Bontridder <dany@alchimerys.be>
2012-01-05 15:20:32 +0000

    Budget : Detail ne fonctionne
    git-svn-id: svn+ssh://ns352270.ovh.net/svn_extension/trunk@252 86b2a579-bf87-4cb0-bb5f-5dc311b2927b

--- coprop/include/template/budget_list.php
+++ coprop/include/template/budget_list.php
@@ -17,7 +17,7 @@ for ($i=0;$i<count($array);$i++):
     $class=($i%2==0)?' class="evend" ':' class="odd" ';
     $str_js=sprintf(" onclick=\"budget_detail('%s','%s','%s','%s')\" ",
             $_REQUEST['plugin_code'],$_REQUEST['ac'],$_REQUEST['gDossier'],$array[$i]['b_id']);
-    $js=HtmlInput::anchor("Détail",$str_js);
+    $js=HtmlInput::anchor("Détail","",$str_js);
 ?>
     <tr <?=$class?> >
         <td>
