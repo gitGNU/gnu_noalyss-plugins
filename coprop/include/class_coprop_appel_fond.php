d0faa226c7193b01f0d18b2bbceb5adc1b3038b3
Dany De Bontridder <dany@alchimerys.be>
2012-03-25 13:54:53 +0000

    Nbre de ligne incorrecte dans appel de fond
    git-svn-id: svn+ssh://ns352270.ovh.net/svn_extension/trunk@313 86b2a579-bf87-4cb0-bb5f-5dc311b2927b

--- coprop/include/class_coprop_appel_fond.php
+++ coprop/include/class_coprop_appel_fond.php
@@ -177,7 +177,7 @@ class Coprop_Appel_Fond
 		$fiche=new Fiche($cn,$this->af_card);
 		$array['qc_0']=$fiche->get_quick_code();
 		$array['amount0']=$this->af_amount;
-		$array['nb_item']=count($adetail);
+		$array['nb_item']=count($adetail)+2;
 
 		for ($i=0;$i<count($adetail);$i++){
 			$idx=$i+1;
