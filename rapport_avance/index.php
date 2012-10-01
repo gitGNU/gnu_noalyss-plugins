7c7472a532d70e0e5d6bed832d50fceb91f810b8
Dany De Bontridder <dany@alchimerys.be>
2012-10-01 21:32:01 +0000

    Adapt to the last version of phpcompta
    git-svn-id: svn+ssh://ns352270.ovh.net/svn_extension/trunk@433 86b2a579-bf87-4cb0-bb5f-5dc311b2927b

--- rapport_avance/index.php
+++ rapport_avance/index.php
@@ -27,7 +27,7 @@
 
 global $version_plugin;
 $version_plugin = SVNINFO;
-Extension::check_version(5082);
+Extension::check_version(5100);
 require_once 'class_phpcompta_sql.php';
 require_once 'rapav_constant.php';
 require_once 'class_impress.php';
