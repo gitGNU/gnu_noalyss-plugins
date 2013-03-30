fb47586931a16194ff9f1a7d22c3110110146dcf
Dany De Bontridder <dany@alchimerys.be>
2013-03-30 20:32:01 +0000

    Negative VAT rate
    git-svn-id: svn+ssh://ns352270.ovh.net/svn_extension/tag/rel652@478 86b2a579-bf87-4cb0-bb5f-5dc311b2927b

--- import_doli/include/class_impdol_operation.php
+++ import_doli/include/class_impdol_operation.php
@@ -383,13 +383,14 @@ class Impdol_Operation
 							break;
 					}
 					/* save VAT into an array */
-					if (isset($tva[$tva_id]))
+					$side=($amount_tva > 0)?1:0;
+					if (isset($tva[$tva_id][$side]))
 					{
-						$tva[$tva_id] = bcadd($tva[$tva_id], $amount_tva);
+						$tva[$tva_id][$side] = bcadd($tva[$tva_id][$side], $amount_tva);
 					}
 					else
 					{
-						$tva[$tva_id] = $amount_tva;
+						$tva[$tva_id][$side] = $amount_tva;
 					}
 					$sum = bcadd($sum, $amount_tvac);
 
@@ -408,27 +409,28 @@ class Impdol_Operation
 				$jtiers->qcode = $oper_tiers->getp("qcode");
 				$jtiers->desc = mb_substr($oper_tiers->getp("desc"),0,80,'UTF8');
 				$jtiers->insert_jrnx();
-
 				/* Record the vat 1 */
-				foreach ($tva as $key => $value)
+				foreach ($tva as $key => $atva)
 				{
-					$tva = new Acc_TVA($cn, $key);
-					$tva->load();
-					$poste = $tva->get_side($oth_side);
-					$op_tva = new Acc_Operation($cn);
-					$op_tva->date = $date;
-					$op_tva->amount = $value;
-					$op_tva->poste = $poste;
-					$op_tva->grpt = $grpt;
-					$op_tva->type = $oth_side;
-					$op_tva->jrn = $jrn;
-					$op_tva->user = $_SESSION['g_user'];
-					$op_tva->periode = 0;
-					$op_tva->qcode = null;
-					$op_tva->desc = $tva->tva_label;
-					$op_tva->insert_jrnx();
+					foreach ($atva as $tvaid=>$tva_value)
+					{
+						$tva = new Acc_TVA($cn, $key);
+						if ( $tva->load() == -1 ) die ("Code TVA inconnu $tvaid");
+						$poste = $tva->get_side($oth_side);
+						$op_tva = new Acc_Operation($cn);
+						$op_tva->date = $date;
+						$op_tva->amount = $tva_value;
+						$op_tva->poste = $poste;
+						$op_tva->grpt = $grpt;
+						$op_tva->type = $oth_side;
+						$op_tva->jrn = $jrn;
+						$op_tva->user = $_SESSION['g_user'];
+						$op_tva->periode = 0;
+						$op_tva->qcode = null;
+						$op_tva->desc = $tva->tva_label;
+						$op_tva->insert_jrnx();
+					}
 				}
-
 				/* record into jrn */
 				$acc_jrn = new Acc_Operation($cn);
 				$acc_jrn->jrn = $jrn;
