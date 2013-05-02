f51482834f14444f94dedb5f4cdb4f9335e17608
Dany De Bontridder <dany@alchimerys.be>
2013-05-02 21:18:59 +0000

    Forgotten file
    git-svn-id: svn+ssh://ns352270.ovh.net/svn_extension/trunk@490 86b2a579-bf87-4cb0-bb5f-5dc311b2927b

---
+++ rapport_avance/include/formulaire_definition_show.inc.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ *   This file is part of PhpCompta.
+ *
+ *   PhpCompta is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   PhpCompta is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with PhpCompta; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+/* $Revision$ */
+
+// Copyright Author Dany De Bontridder ddebontridder@yahoo.fr
+
+/**
+ * @file
+ * @brief
+ *
+ */
+echo '<h1>Définition </h1>';
+$form = new RAPAV_formulaire($_REQUEST['f_id']);
+$form->load_definition();
+echo '<form method="POST" enctype="multipart/form-data" class="print">';
+$form->input_formulaire();
+$form->input_definition();
+echo '<p>';
+$delete = new ICheckBox('delete');
+echo "Cochez la case pour effacer ce formulaire " . $delete->input();
+echo '</p>';
+echo HtmlInput::submit('form_def_sub', 'Sauve');
+echo '</form>';
+?>
