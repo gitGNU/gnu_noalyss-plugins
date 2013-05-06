c354f92c67e7b7df1e6d18a6534e2375b2b1191a
Dany De Bontridder <dany@alchimerys.be>
2013-05-06 23:50:07 +0000

    Minimum version is 5220
    git-svn-id: svn+ssh://ns352270.ovh.net/svn_extension/trunk@493 86b2a579-bf87-4cb0-bb5f-5dc311b2927b

--- rapport_avance/index.php
+++ rapport_avance/index.php
@@ -27,7 +27,7 @@
 
 global $version_plugin;
 $version_plugin = SVNINFO;
-Extension::check_version(5100);
+Extension::check_version(5220);
 require_once 'class_phpcompta_sql.php';
 require_once 'rapav_constant.php';
 require_once 'class_impress.php';
