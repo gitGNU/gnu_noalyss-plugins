2c76c0b61232286683b1e7edcbcb76a21bc599e9
Dany De Bontridder <dany@alchimerys.be>
2013-12-20 15:08:42 +0000

    Add copy to the sender
    git-svn-id: svn+ssh://ns352270.ovh.net/svn_extension/trunk@635 86b2a579-bf87-4cb0-bb5f-5dc311b2927b

--- rapport_avance/ajax_send_mail.php
+++ rapport_avance/ajax_send_mail.php
@@ -4,7 +4,9 @@ require_once 'include/class_rapav_listing_compute.php';
 ob_start();
 $listing=new RAPAV_Listing_Compute();
 $listing->load($lc_id);
-$a_result=$listing->send_mail($p_from,$p_subject,$p_message,$p_attach);
+$tcopy=(isset($copy))?1:0;
+
+$a_result=$listing->send_mail($p_from,$p_subject,$p_message,$p_attach,$tcopy);
 echo HtmlInput::title_box(_('Résultat'),'parameter_send_mail_result');
 echo '<ol>';
 for ($i=0;$i<count($a_result);$i++)
