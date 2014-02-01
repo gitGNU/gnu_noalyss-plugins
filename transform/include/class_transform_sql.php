4cd00d59b8861bea3fb606e9b8a2b7c7f6a370d7
Dany De Bontridder <dany@alchimerys.be>
2014-02-01 15:15:52 +0000

    add column r_type to table request
    git-svn-id: svn+ssh://ns352270.ovh.net/svn_extension/trunk@679 86b2a579-bf87-4cb0-bb5f-5dc311b2927b

--- transform/include/class_transform_sql.php
+++ transform/include/class_transform_sql.php
@@ -36,6 +36,7 @@ class Transform_Request_SQL extends Phpcompta_SQL
     //------ Attributes-----
     var $r_id;
     var $r_date;
+    var $r_type;
 
     /* example private $variable=array("easy_name"=>column_name,"email"=>"column_name_email","val3"=>0); */
 
@@ -48,12 +49,14 @@ class Transform_Request_SQL extends Phpcompta_SQL
 
         $this->name = array(
             "r_id" => "r_id",
-            "r_date" => "r_date"
+            "r_date" => "r_date",
+            "r_type"=>'r_type'
         );
 
         $this->type = array(
             "r_id" => "numeric",
-            "r_date" => "date"
+            "r_date" => "date",
+            "r_type"=>"text"
         );
 
         $this->default = array(
