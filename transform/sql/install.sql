d9d4ad96c35999e203aef67f3c73febc2d94793b
Dany De Bontridder <dany@alchimerys.be>
2014-02-08 13:49:01 +0000

    Correct install script
    git-svn-id: svn+ssh://ns352270.ovh.net/svn_extension/trunk@703 86b2a579-bf87-4cb0-bb5f-5dc311b2927b

--- transform/sql/install.sql
+++ transform/sql/install.sql
@@ -1,6 +1,11 @@
-﻿create schema transform;
+begin;
+create schema transform;
 create table transform.request(
-	r_id bigserial primary key
+	r_id bigserial primary key,
+        r_date date default now(),
+        r_type text,
+        r_start_date date ,
+        r_end_date date 
 	);
 create table transform.version(
 	v_id bigint primary key,
@@ -18,39 +23,34 @@ create table transform.intervat_representative
 	rp_city text,
 	rp_email text,
 	rp_phone text,
-	rp_countrycode char(2) not null
+	rp_countrycode char(2) default null
 
 );
 create table transform.intervat_declarant
 (
 	d_id bigserial primary key,
 	r_id bigint references transform.request(r_id) on update cascade on delete cascade,
-	d_name text,
-	d_street text,
-	d_postcode text,
-	d_city text,
-	d_email text,
-	d_phone text,
-	d_vat_number text,
+	d_name text not null,
+	d_street text not null,
+	d_postcode text not null,
+	d_city text not null,
+	d_email text not null,
+	d_phone text not null,
+	d_vat_number text not null,
 	d_countrycode char(2) not null,
-	d_periode text
+	d_periode text not null
 );
 create table transform.intervat_client
 (
 	c_id bigserial primary key,
 	d_id bigint references transform.intervat_declarant on update cascade on delete cascade,
 	c_name text,
+        c_comment text,
 	c_vatnumber text,
-	c_amount_vat text,
-	c_amount_novat text,
+	c_amount_vat text default '0',
+	c_amount_novat text default '0',
 	c_issuedby char(2) default 'BE'
-)
+);
 
-alter table transform.request add         r_date date;
-alter table transform.request alter r_date set default now();
-alter table transform.request add         r_type text ;
-ALTER TABLE transform.request ADD COLUMN r_start_date date;
-ALTER TABLE transform.request ADD COLUMN r_end_date date;
-alter table transform.intervat_client add column c_comment text;
-alter table transform.intervat_representative alter rp_countrycode drop not null;
-
+insert into transform.version values(1,'Installation plugin');
+commit;
