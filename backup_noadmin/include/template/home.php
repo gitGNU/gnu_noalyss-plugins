0c88ac765eb0ebe7734fc0fb6866c3aac5fcce08
Dany De Bontridder <dany@alchimerys.be>
2014-02-13 19:37:36 +0000

    Translation backup
    git-svn-id: svn+ssh://ns352270.ovh.net/svn_extension/branches/rel672n@711 86b2a579-bf87-4cb0-bb5f-5dc311b2927b

--- backup_noadmin/include/template/home.php
+++ backup_noadmin/include/template/home.php
@@ -1,6 +1,6 @@
-<h2> Backup du dossier courant </h2>
+<h2><?php echo _("Backup du dossier courant")?> </h2>
 <form method="get" ACTION="extension.raw.php">
-Voulez-vous avoir une copie de ce dossier
+   <?php echo _("Voulez-vous avoir une copie de ce dossier")?>
 <?php echo HtmlInput::submit('backup','backup')?>
 <?php echo dossier::hidden()?>
 <?php echo HtmlInput::extension();?>
