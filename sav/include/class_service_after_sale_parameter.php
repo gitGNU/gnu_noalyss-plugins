26baf972a9f3ee57a6693e5491b617c67b634e5e
Dany De Bontridder <dany@alchimerys.be>
2015-05-14 17:35:35 +0200

    set Parameter

--- sav/include/class_service_after_sale_parameter.php
+++ sav/include/class_service_after_sale_parameter.php
@@ -19,7 +19,7 @@
  */
 /* $Revision$ */
 
-// Copyright (c) 2002 Author Dany De Bontridder dany@alchimerys.be
+// Copyright (c) 2015 Author Dany De Bontridder dany@alchimerys.be
 
 /**
  * @file
@@ -28,8 +28,10 @@
  */
 class Service_After_Sale_Parameter
 {
-    private $good;  // Parameter for returned material
-    private $spare; // parameter for spare part
+    private $good;  //!< Parameter for returned material : list separated by comma of fd_id
+    private $spare; //!<  parameter for spare part : : list separated by comma of fd_id
+    private $ledger ; //!<  ledger id (jrn_def.jrn_def_id)
+    private $workhour; //!< card id for the workhour 
     
     
     //
@@ -59,22 +61,22 @@ class Service_After_Sale_Parameter
     {
 		global $cn;
         // check if the parameter does exist
-        if ( $this->cn->get_value('select count(*) from service_after_sale.sav_parameter where code=$1',array($p_option)) != 0 )
+        if ( $cn->get_value('select count(*) from service_after_sale.sav_parameter where code=$1',array($p_option)) != 0 )
         {
-            $Res=$this->cn->exec_sql("update service_after_sale.sav_parameter set value=$1 where code=$2",
+            $Res=$cn->exec_sql("update service_after_sale.sav_parameter set value=$1 where code=$2",
                                      array($p_value,$p_option));
         }
         else
         {
 
-            $Res=$this->cn->exec_sql("insert into service_after_sale.sav_parameter (code,value) values( $1,$2)",
+            $Res=$cn->exec_sql("insert into service_after_sale.sav_parameter (code,value) values( $1,$2)",
                                      array($p_option,$p_value));
 
         }
 
     }
     /**
-     * return list of materials, value empty or null is an error
+     * return list of category of card for materials, separated by comma, value empty or null is an error
      * @return string
      */
     function get_material()
@@ -94,29 +96,100 @@ class Service_After_Sale_Parameter
             
         $this->good=$p_material;
     }
+    /**
+     * return list of category of card for materials, , separated by comma, value empty or null is an error
+     * @return string
+     */
     function get_spare_part()
     {
-        return '1,2,3';
+        
         return $this->spare;
         
     }
+     /**
+     * Set the string for spare_part and verify the string is valid, the string
+     * must be not empty, must contains existing fd_id and separated by comma, not other
+     * characters than comma and number.
+     * @param $p_spare string
+     * @exception  : no material
+      * 
+      * @return string
+      */
     function set_spare_part($p_spare)
     {
         if (trim($p_spare)=="")                throw new Exception (_('Aucun matériel choisi'),NOMATERIAL);
             
         $this->spare=$p_spare;
     }
-    function get_workhour_qcode()
+    function get_workhour()
     {
-        return 1;
+        return $this->workhour;
     }
-    function set_workhour_qcode($p_string)
+    function set_workhour($p_workhour)
     {
-        $this->workhour_qcode=$p_string;
+        $this->workhour=$p_workhour;
     }
+    /**
+     * returns ledger id
+     */
     function get_ledger()
     {
-        return 1;
+        return $this->ledger;
+    }
+    function set_ledger($p_ledger)
+    {
+        $this->ledger=$p_ledger;
+    }
+    /**
+     * Display list of available ledgers of sales
+     * @return  HTML select string 
+     */
+    function input_ledger()
+    {
+        $ledger=new Acc_Ledger($this->db,$this->ledger);
+        $select= $ledger->select_ledger('VEN',3);
+        $select->name='ledger';
+        return $select->input();
+    }
+    
+    /**
+     * Display current card id and propose to update it
+     * @return \ICard
+     */
+    function input_workhour()
+    {
+        global $cn;
+      $select=new ISelect('workhour_select');
+      $select->value=$cn->make_array('select fd_id, fd_label from fiche_def order by fd_label');
+      $select->selected=$this->get_workhour();
+      return $select->input();
+        
+    }
+    /**
+     * Display the card category used for the spare part
+     * @return \ICard
+     */
+
+    function input_spare_part()
+    {
+           global $cn;
+      $select=new ISelect('spare_part_select');
+      $select->value=$cn->make_array('select fd_id, fd_label from fiche_def order by fd_label');
+      $select->selected=$this->get_spare_part();
+      return $select->input();
+    }
+    /**
+     * Display and input the card category used for the good
+     * @return \ICard
+     */
+
+    function input_good()
+    {
+           global $cn;
+      $select=new ISelect('good_select');
+      $select->value=$cn->make_array('select fd_id, fd_label from fiche_def order by fd_label');
+      $select->selected=$this->get_material();
+      return $select->input();
     }
 }   
 ?>
---
+++ sav/include/sav_param.inc.php
@@ -0,0 +1,99 @@
+<?php
+/*
+ *   This file is part of PhpCompta.
+ *
+ *   PhpCompta is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   PhpCompta is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with PhpCompta; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+// Copyright (2015) Author Dany De Bontridder <dany@alchimerys.be>
+
+if ( ! defined ('ALLOWED') ) die('Appel direct ne sont pas permis');
+
+/**
+ * @file
+ * @brief 
+ * @param Manage the table sav_parameter, display and set the parameter for the
+ * table sav_parameter
+ */
+require_once 'class_service_after_sale_parameter.php';
+
+global $g_sav_parameter;
+$save=HtmlInput::default_value_post('save_param', '');
+if ($save != ' ')
+{
+    /**
+     * Save the parameters
+     */
+    $spare=HtmlInput::default_value_post('spare_part_select',-1);
+    $ledger=HtmlInput::default_value_post('ledger',-1);
+    $workhour=HtmlInput::default_value_post('workhour_select',-1);
+    $good=HtmlInput::default_value_post('good_select',-1);
+    if ( $ledger == -1 )        throw new Exception(_('Paramètre invalide'));
+    if ( $spare == -1 )        throw new Exception(_('Paramètre invalide'));
+    if ( $workhour == -1 )        throw new Exception(_('Paramètre invalide'));
+    if ( $good == -1 )        throw new Exception(_('Paramètre invalide'));
+    $g_sav_parameter->set_material($good);
+    $g_sav_parameter->save('good',$good);
+    $g_sav_parameter->set_spare_part($spare);
+    $g_sav_parameter->save('spare',$spare);
+    $g_sav_parameter->set_ledger($ledger);
+    $g_sav_parameter->save('ledger',$ledger);
+    $g_sav_parameter->set_workhour($workhour);
+    $g_sav_parameter->save('workhour',$workhour);
+    var_dump($_POST);
+}
+
+?>
+<form method="post">
+    <?php
+        echo HtmlInput::array_to_hidden(array('ac','gDossier','plugin_code'),$_REQUEST);
+    ?>
+<table>
+    <tr>
+        <td>
+        <?php echo _('Journal')?>
+        </td>
+        <td >
+            <?php echo $g_sav_parameter->input_ledger()?>
+        </td>
+    </tr>
+    <tr>
+        <td>
+        <?php echo _('Fiche pour les heure')?>
+        </td>
+        <td>
+            <?php echo $g_sav_parameter->input_workhour()?>
+        </td>
+    </tr>
+    <tr>
+        <td>
+        <?php echo _('Catégorie de fiche pour les pièces')?>
+        </td>
+        <td>
+            <?php echo $g_sav_parameter->input_spare_part()?>
+        </td>
+    </tr>
+    <tr>
+        <td>
+        <?php echo _('Catégorie des biens à dépanner')?>
+        </td>
+        <td>
+            <?php echo $g_sav_parameter->input_good()?>
+        </td>
+    </tr>
+</table>
+    <?php echo HtmlInput::submit('save_param', _('Sauver les paramètres'));?>
+</form>
+    
+    
