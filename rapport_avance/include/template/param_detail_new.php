cc76ed7b69e86d0d6150527b4fc5afc2fa17debc
Dany De Bontridder <dany@alchimerys.be>
2015-06-23 18:46:08 +0200

    esthétique : emplacement bouton sauve

--- rapport_avance/include/template/param_detail_new.php
+++ rapport_avance/include/template/param_detail_new.php
@@ -78,8 +78,8 @@
 		<?php echo RAPAV_Formula::new_row()?>
 	</p>
        
-<?php echo HtmlInput::submit('save','Sauve')?>
-
+<?php echo HtmlInput::submit('save','Sauve','style="   position: absolute;
+    bottom: 10px"')?>
 </form>
 </div>
 <div id="new_account_tva_id" style="display: none">
@@ -92,7 +92,8 @@
 	<?php echo HtmlInput::hidden('tab','account_tva')?>
 		<?php echo RAPAV_Account_Tva::new_row()?>
  
-<?php echo HtmlInput::submit('save','Sauve')?>
+<?php echo HtmlInput::submit('save','Sauve','style="   position: absolute;
+    bottom: 10px"')?>
 
 </form>
 </div>
@@ -105,9 +106,9 @@
 
 	<?php echo HtmlInput::hidden('tab','compute_id')?>
 		<?php echo RAPAV_Compute::new_row($p_id)?>
-    <p>
-<?php echo HtmlInput::submit('save','Sauve')?>
-    </p>
+    <p >
+<?php echo HtmlInput::submit('save','Sauve','style="   position: absolute;
+    bottom: 10px"')?>    </p>
 
 </form>
 </div>
@@ -119,8 +120,8 @@
 	<?php echo HtmlInput::hidden('tab','new_account_id')?>
 	<?php echo RAPAV_Account::new_row($p_id)?>
         
-<?php echo HtmlInput::submit('save','Sauve')?>
-
+<?php echo HtmlInput::submit('save','Sauve','style="   position: absolute;
+    bottom: 10px"')?>
 </form>
 </div>
 		<div id="new_reconcile_id" style="display: none">
@@ -131,8 +132,8 @@
 	<?php echo HtmlInput::hidden('tab','new_reconcile_id')?>
 		<?php echo RAPAV_Reconcile::new_row($p_id)?>
   
-<?php echo HtmlInput::submit('save','Sauve')?>
-
+<?php echo HtmlInput::submit('save','Sauve','style="   position: absolute;
+    bottom: 10px"')?>
 </form>
 </div>
 	</div>
