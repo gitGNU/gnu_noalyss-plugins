af48b563fef95d1bebe446adb4b86569ab2d8c4a
Dany De Bontridder <dany@alchimerys.be>
2016-02-18 17:25:16 +0100

    Voir le modèle de document dans liste des listing

--- rapport_avance/include/template/rapav_listing_to_list.php
+++ rapport_avance/include/template/rapav_listing_to_list.php
@@ -11,6 +11,9 @@
             Description
         </th>
         <th>
+            Modèle de document
+        </th>
+        <th>
             Catégorie de fiches
         </th>
     </tr>
@@ -28,7 +31,26 @@
             'cin' => 'listing_definition_id',
             'cout' => 'listing_definition_div_id');
         $json = 'listing_definition(' . str_replace('"', "'", json_encode($arg)) . ')';
+        
+        $arg2=array(
+            'gDossier' => Dossier::id(),
+            'ac' => $_REQUEST['ac'],
+            'pc' => $_REQUEST['plugin_code'],
+            'id' => $row['l_id'],
+            'cin' => 'listing_detail_id',
+            'cout' => 'listing_detail_id');
+        $modify = 'listing_modify('.str_replace('"',"'",  json_encode($arg2).')');
+        $url_document="extension.raw.php?".http_build_query(array(
+            'gDossier' => Dossier::id(),
+            'ac' => $_REQUEST['ac'],
+            'plugin_code' => $_REQUEST['plugin_code'],
+            'act'=>'downloadTemplateListing',
+            "id"=>$row['l_id']
+           ));
     ?>
+    <a class="line"href="extension.raw.php?gDossier=10077&amp;ac=rapav&amp;plugin_code=rapav&amp;id=1&amp;act=downloadTemplateListing" >00.m3u</a>
+    <a class="line" href="extension.raw.php?gDossier=10077&amp;ac=EXT%2FRAPAV&amp;plugin_code=EXT%2FRAPAV&amp;act=downloadTemplateListing&amp;id=1">00.m3u</a>
+    
     	<tr <?php echo $class; ?>>
         <td>
             <?php echo HtmlInput::anchor(h($row['l_name']),'',' onclick="'.$json.'"'); ?>
@@ -37,9 +59,13 @@
             <?php echo h($row['l_description']); ?>
         </td>
         <td>
+           <?php echo HtmlInput::anchor(h($row['l_filename']), $url_document);?>
+        </td>
+        <td>
             <?php echo h($row['fd_label']); ?>
         </td>
         <td>
+            <?php echo HtmlInput::anchor(_('Modifie'),'',' onclick="'.$modify.'"'); ?>
             
         </td>
     </tr>
